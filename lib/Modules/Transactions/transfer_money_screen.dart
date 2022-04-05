bdd8095defbe78e39512acaf8c8fb8ea8b0bd938
Ilhan-brandverse-dev <102786877+Ilhan-brandverse-dev@users.noreply.github.com>
2022-04-05 08:43:00 +0500

    padding and margin values fixed

--- lib/Modules/Transactions/transfer_money_screen.dart
+++ lib/Modules/Transactions/transfer_money_screen.dart
@@ -5,7 +5,6 @@ import 'package:test_project/Modules/Transactions/widgets/receiver_card.dart';
 import 'package:test_project/Utils/color_codes.dart';
 import 'package:test_project/Utils/utils.dart';
 
-
 class TransferMoneyScreen extends StatefulWidget {
   const TransferMoneyScreen({Key? key}) : super(key: key);
 
@@ -14,7 +13,7 @@ class TransferMoneyScreen extends StatefulWidget {
 }
 
 class _TransferMoneyScreenState extends State<TransferMoneyScreen> {
-  double amountToTransfer=0.0;
+  double amountToTransfer = 0.0;
   double min = 0.0;
   double max = 400;
   @override
@@ -26,18 +25,13 @@ class _TransferMoneyScreenState extends State<TransferMoneyScreen> {
         child: AppBar(
           backgroundColor: ColorCodes.DARK_PURPLE_COLOR,
           elevation: 0,
-          iconTheme: const IconThemeData(
-            color: ColorCodes.WHITE_COLOR
+          iconTheme: const IconThemeData(color: ColorCodes.WHITE_COLOR),
+          title: const Text(
+            "Transfer Money",
+            style: TextStyle(color: ColorCodes.WHITE_COLOR, fontSize: 20),
           ),
-          title: const Text("Transfer Money",
-            style: TextStyle(
-              color: ColorCodes.WHITE_COLOR,
-            fontSize: 20
-          ),),
-
         ),
       ),
-
       body: SafeArea(
         child: Container(
           height: Utils.getScreenHeight(context),
@@ -45,7 +39,9 @@ class _TransferMoneyScreenState extends State<TransferMoneyScreen> {
           child: Column(
             children: [
               headerWidget(),
-              const SizedBox(height: 15,),
+              const SizedBox(
+                height: 15,
+              ),
               bodyContent()
             ],
           ),
@@ -54,19 +50,22 @@ class _TransferMoneyScreenState extends State<TransferMoneyScreen> {
     );
   }
 
-  headerWidget(){
+  headerWidget() {
     return Expanded(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
         child: Column(
           children: const [
-            Text("SELECT YOUR BALANCE",
+            Text(
+              "SELECT YOUR BALANCE",
               style: TextStyle(
-              color: ColorCodes.WHITE_COLOR,
-              fontSize: 16,
-              letterSpacing: 2
-            ),),
-            SizedBox(height: 10,),
+                  color: ColorCodes.WHITE_COLOR,
+                  fontSize: 16,
+                  letterSpacing: 2),
+            ),
+            SizedBox(
+              height: 10,
+            ),
             MasterCard()
           ],
         ),
@@ -74,84 +73,106 @@ class _TransferMoneyScreenState extends State<TransferMoneyScreen> {
     );
   }
 
-  bodyContent(){
+  bodyContent() {
     return Expanded(
       flex: 2,
       child: Container(
-        padding:const  EdgeInsets.symmetric(horizontal: 15,vertical: 12),
-        decoration:const BoxDecoration(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        decoration: const BoxDecoration(
             color: ColorCodes.WHITE_COLOR,
             borderRadius: BorderRadius.only(
-                topRight: Radius.circular(35),
-                topLeft: Radius.circular(35)
-            )
-        ),
-        child: Column(
+                topRight: Radius.circular(35), topLeft: Radius.circular(35))),
+        child: ListView(
           children: [
-            const SizedBox(height: 12,),
-           const Text("RECEIVER CARD NUMBER",
-              style: TextStyle(
-              fontSize: 15,
-              color: ColorCodes.BLACK_COLOR,
-              fontWeight: FontWeight.w600,
-            ),),
-            const SizedBox(height: 12,),
+            const SizedBox(
+              height: 12,
+            ),
+            const Center(
+              child: Text(
+                "RECEIVER CARD NUMBER",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: ColorCodes.BLACK_COLOR,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
             const ReceiverCard(),
-           const SizedBox(height: 25,),
-            const Text("AMOUNT TO TRANSFER",
-              style: TextStyle(
-                fontSize: 15,
-                color: ColorCodes.BLACK_COLOR,
-                fontWeight: FontWeight.w600,
-              ),),
-            const SizedBox(height: 15,),
-            Text("\$ $amountToTransfer",style:const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 32,
-            ),),
-            const SizedBox(height: 15,),
+            const SizedBox(
+              height: 25,
+            ),
+            const Center(
+              child: Text(
+                "AMOUNT TO TRANSFER",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: ColorCodes.BLACK_COLOR,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Center(
+              child: Text(
+                "\$ $amountToTransfer",
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
             sliderWidget(),
-            const SizedBox(height: 15,),
+            const SizedBox(
+              height: 15,
+            ),
             AmountSelector(
                 min: min,
                 max: max,
                 division: 8,
-                onChange: (val){
+                onChange: (val) {
                   setState(() {
                     amountToTransfer = val;
                   });
-                }
+                }),
+            const SizedBox(
+              height: 30,
             ),
-            const Spacer(),
             SizedBox(
               height: 60,
               width: double.maxFinite,
               child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  elevation: 0,
-                  primary: ColorCodes.PEACH_COLOR.withOpacity(0.8),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25)
-                  )
-                ),
-                  onPressed: (){},
-                  child: Text("Transfer  \$$amountToTransfer",
-                  style: const TextStyle(
-                    color: ColorCodes.BLACK_COLOR,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold
-                  ),)
-              ),
+                  style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      primary: ColorCodes.PEACH_COLOR.withOpacity(0.8),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25))),
+                  onPressed: () {},
+                  child: Text(
+                    "Transfer  \$$amountToTransfer",
+                    style: const TextStyle(
+                        color: ColorCodes.BLACK_COLOR,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  )),
             ),
-            const SizedBox(height: 20,)
-
+            const SizedBox(
+              height: 20,
+            )
           ],
         ),
       ),
     );
   }
 
-  sliderWidget(){
+  sliderWidget() {
     return Column(
       children: [
         Slider(
@@ -165,18 +186,17 @@ class _TransferMoneyScreenState extends State<TransferMoneyScreen> {
               setState(() {
                 amountToTransfer = val;
               });
-            }
-        ),
+            }),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text("\$ $min",style: TextStyle(
-              color: ColorCodes.BLACK_COLOR.withOpacity(0.5)
-            ),),
-            Text("\$ $max",style: TextStyle(
-                color: ColorCodes.BLACK_COLOR.withOpacity(0.5)
-            )
-            )
+            Text(
+              "\$ $min",
+              style: TextStyle(color: ColorCodes.BLACK_COLOR.withOpacity(0.5)),
+            ),
+            Text("\$ $max",
+                style:
+                    TextStyle(color: ColorCodes.BLACK_COLOR.withOpacity(0.5)))
           ],
         )
       ],
